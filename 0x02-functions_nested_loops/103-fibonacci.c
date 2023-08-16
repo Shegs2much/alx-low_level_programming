31b10ea887d45ab4d3cc0d34f7986fa126b47222
Shegs2much <seguntemenu@gmail.com>
2023-08-16 09:18:01 -0700

    Nested loops update

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 
-	/**
-	 * main - finds and prints the sum of the even-valued terms
-	 * followed by a new line
-	 * Return: Always 0 (Success)
-	 */
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 	int main(void)
 	{
 		int i;
@@ -33,4 +33,4 @@
 
 
 		return (0);
-	}
+}
