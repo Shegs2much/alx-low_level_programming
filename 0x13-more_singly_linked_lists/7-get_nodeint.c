437d91449c7535055e7f47ca72d246afe2cc9315
Shegs2much <seguntemenu@gmail.com>
2023-09-26 07:05:31 -0700

    update more singly

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * get_nodeint_at_index - this returns the node at a certain index in a linked list
  * @head: first node in the linked list
- * @index: index of the node to return
+ * @index: index of the node to return .
  *
  * Return: pointer to the node we're looking for, or NULL
  */
