a4d0fca3b5e9616afe0b328136c49da60920a3c3
Shegs2much <seguntemenu@gmail.com>
2023-10-02 08:32:10 -0700

    cfile oi

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@ void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The E name of the file buffer is storing chars for.
+ * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
@@ -46,13 +46,13 @@ void close_file(int fd)
 
 /**
  * main - Copies the contents of a file to another file.
- * @argc: The no/number of arguments supplied to the program.
+ * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success.
  *
- * Description: If this  argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read read - exit code 98.
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
  *              If file_to or file_from cannot be closed - exit code 100.
  */
